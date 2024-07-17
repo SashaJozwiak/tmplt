06d5d14698b7d4a5c0a58ddae888e9cb0358aa17
SashaJozwiak <wmdz88@gmail.com>
2024-07-17 17:28:21 +0300

    feat: tmplt solution

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.css'
 
 WebApp.ready();
 
-const manifestUrl = 'https://sashajozwiak.github.io/***/tonconnect-manifest.json';
+const manifestUrl = 'https://sashajozwiak.github.io/tmplt/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
